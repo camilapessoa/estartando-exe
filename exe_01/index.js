f2db07f8d7e290915ffc2b3ccad95eb2ee285c57
camilapessoa <camilabentespessoa@gmail.com>
2021-06-10 21:27:27 -0300

    teste

---
+++ exe_01/index.js
@@ -0,0 +1,13 @@
+//pegar idade e converter para dias, 365 dias e mês com 30 dias
+
+//nomes de funções remetem a ações, por isso utilizar verbo no infinitivo
+
+ConverterIdade = (idade, dias) => {
+  const idadeData = new Date(idade).getDay
+  console.log(idadeData)
+
+}
+
+console.log(ConverterIdade(12))
+
+//teste se tá subindo pro github
